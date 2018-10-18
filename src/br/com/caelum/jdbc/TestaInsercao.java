2ecefa2981196285ff9c037c47bf9bbf4bf88ec6
machap <paulinhodj10@gmail.com>
2018-10-18 10:19:37 -0300

    Refatoração da classe testeInserção

--- src/br/com/caelum/jdbc/TestaInsercao.java
+++ src/br/com/caelum/jdbc/TestaInsercao.java
@@ -1,6 +1,7 @@
 package br.com.caelum.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,20 +10,45 @@ public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
 
-		Connection connection = Database.getConnection();
-		Statement statement = connection.createStatement();
-		boolean resultado = statement.execute(
-				"insert into produto (nome, descricao) values ('Notebook', 'Notebook i5')",
-				Statement.RETURN_GENERATED_KEYS);
-		ResultSet generatedKeys = statement.getGeneratedKeys();
-		
-		while (generatedKeys.next()) {
-			long id = generatedKeys.getLong("id");
-			System.out.println("id gerado: " + id);
+		try (Connection connection = Database.getConnection()) {
+			
+			connection.setAutoCommit(false);
+			
+			String sql = "insert into produto (nome, descricao) values (?, ?)";
+
+			try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+				adiciona("Notebook", "HP i5 500gb", statement);
+				adiciona("Blueray", "HDMI Full", statement);
+				
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				connection.rollback();
+				System.out.println("Rollback efetuado");
+			}
+
 		}
-		generatedKeys.close();
-		statement.close();
-		connection.close();
+	}
+
+	private static void adiciona(String nome, String descricao, PreparedStatement statement) throws SQLException {
+		if (nome.equals("Blueray")) {
+			throw new IllegalArgumentException("Problema ocorrido");
+		}
+
+		statement.setString(1, nome);
+		statement.setString(2, descricao);
+
+		boolean resultado = statement.execute();
+		System.out.println(resultado);
+
+		try (ResultSet result = statement.getGeneratedKeys()) {
+
+			while (result.next()) {
+				long id = result.getLong("id");
+				System.out.println("id gerado: " + id);
+			}
+		}
 	}
 
 }
